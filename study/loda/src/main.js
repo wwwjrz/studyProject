02715c7a07e9d79d4a6b998253633c7d256eab08
jiangrongzhi123 <staids@126.com>
2023-02-23 08:59:28 +0800

    删除项目

--- study/loda/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import _ from 'lodash'
-
-createApp(App).use(store).use(router).use(_).mount('#app')
--- study/loda/src/views/login/login.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>登录页</div>
-</template>
-<script setup>
-  let arr = ['a', 'b', 'c', 'd', 'e']
-  let a = _.chunk(arr, 2)
-  console.log(a)
-</script>
